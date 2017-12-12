6e7c82e0c31501b62a4b745a3d01f046c0cdd71c
Marcin Ciborowski <ciborowskim@outlook.com>
2017-12-13 00:12:29 +0100

    Async botnet start/stop | close #6

--- sshbot/botnet.py
+++ sshbot/botnet.py
@@ -1,5 +1,6 @@
 from subprocess import call
 import argparse
+import asyncio
 
 class BotnetConfig:
 	def __init__(self, bot_count, host, port):
@@ -14,8 +15,17 @@ def parse_config(configline):
 		config_args[1], 
 		int(config_args[2]) if len(config_args) == 3 else 22)
 
-def run_botnet():
+async def start_bot(botId, config, args):
+	proc = await asyncio.create_subprocess_exec(
+		"docker", "run", "-d", "--name", args.container + str(botId), args.image, 
+		"python", "bot.py", config.host, "-p", str(config.port))
+	returncode = await proc.wait()
+	print(returncode)
+
+def run_botnet(args):
 	botId = 0
+	loop = asyncio.get_event_loop()
+	tasks = []
 	with open("botnet.config", "r") as f:
 		configline = f.readline()
 		while configline != "":
@@ -23,12 +33,34 @@ def run_botnet():
 
 			for i in range(0, config.bot_count):
 				botId += 1
-				result = call(
-					["docker", "run", "-d", "--name", args.container + str(botId), args.image, 
-					"python", "bot.py", config.host, "-p", str(config.port)])
-				print(result)
+				tasks.append(asyncio.ensure_future(start_bot(botId, config, args)))
 
 			configline = f.readline()
+
+	loop.run_until_complete(asyncio.gather(*tasks))
+	loop.close()
+
+async def stop_bot(container, botId):
+	cntnr = container + str(botId)
+	proc = await asyncio.create_subprocess_exec("docker", "stop", cntnr)
+	returncode = await proc.wait()
+	print(returncode)
+	proc2 = await asyncio.create_subprocess_exec("docker", "rm", cntnr)
+	returncode2 = await proc2.wait()
+	print(returncode2)
+
+def stop_botnet(args):
+	if args.bots <= 0:
+		raise Exception("Bots number must be greater then 0")
+
+	loop = asyncio.get_event_loop()
+	tasks = []
+	for i in range(1, args.bots+1):
+		tasks.append(asyncio.ensure_future(stop_bot(args.container, i)))
+	loop.run_until_complete(asyncio.gather(*tasks))
+	loop.close()
+
+
 
 parser = argparse.ArgumentParser()
 parser.add_argument("--stop", type=bool, default=False)
@@ -38,13 +70,6 @@ parser.add_argument("-c","--container", type=str, default="sshbot")
 args = parser.parse_args();
 
 if args.stop == False:
-	run_botnet()
+	run_botnet(args)
 else:
-	if args.bots <= 0:
-		raise Exception("Bots number must be greater then 0")
-
-	for i in range(1, args.bots+1):
-		call(["docker", "stop", args.container +str(i)])
-		call(["docker", "rm", args.container + str(i)])
-
-
+	stop_botnet(args)
