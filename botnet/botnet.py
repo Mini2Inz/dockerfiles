9eb28f4013768f7b6e0ebcb92c683915b7a90447
Marcin Ciborowski <ciborowskim@outlook.com>
2018-01-17 01:05:14 +0100

    sshbot reads hosts from file and sleeps between "attacks"

--- botnet/botnet.py
+++ botnet/botnet.py
@@ -18,33 +18,28 @@ def parse_config(configline):
         config_args[1],
         int(config_args[2]) if len(config_args) == 3 else 22)
 
-async def start_bot(botId, config, args):
-    if args.bash:
-        proc = await asyncio.create_subprocess_exec(
-            "docker", "run", "-d", "--name", args.container + str(botId), args.image,
-            "/bot/bot.sh", "root@"+config.host, "-p", str(config.port))
-    else:
-        proc = await asyncio.create_subprocess_exec(
-            "docker", "run", "-d", "--name", args.container + str(botId), args.image,
-            "python", "bot.py", config.host, "-p", str(config.port))
+async def start_bot(bot_id, args):
+    proc = await asyncio.create_subprocess_exec(
+        "docker", "run", "-d", "--name", args.container + str(bot_id), args.image)
     code = await proc.wait()
-    if code != 0: print(str(botId) + " returned " + str(code))
+    if code != 0:
+        print(str(bot_id) + " returned " + str(code))
 
 def run_botnet(args):
-    botId = args.id
+    bot_id = args.id
     loop = asyncio.get_event_loop()
     tasks = []
-    with open("botnet.config", "r") as f:
-        configline = f.readline()
-        while configline != "":
-            config = parse_config(configline)
+    # with open("botnet.config", "r") as f:
+    #     configline = f.readline()
+    #     while configline != "":
+    #         config = parse_config(configline)
 
-            for i in range(0, config.bot_count):
-                if args.sleeptime > 0.0 and (botId - 1)%5 == 0: sleep(args.sleeptime)
-                botId += 1
-                tasks.append(asyncio.ensure_future(start_bot(botId, config, args)))
+    for i in range(0, args.bots):
+        if args.sleeptime > 0.0 and (bot_id - 1)%5 == 0: sleep(args.sleeptime)
+        bot_id += 1
+        tasks.append(asyncio.ensure_future(start_bot(bot_id, args)))
 
-            configline = f.readline()
+    # configline = f.readline()
 
     loop.run_until_complete(asyncio.gather(*tasks))
     loop.close()
@@ -58,26 +53,32 @@ def stop_bots(containers):
     call(containers)
 
 def stop_botnet(args):
-    if args.bots <= 0:
+    if args.stop <= 0:
         raise Exception("Bots number must be greater then 0")
 
     cntnrs = []
-    for i in range(1, args.bots+1):
+    for i in range(1, args.stop+1):
         cntnrs.append(args.container + str(i))
 
     stop_bots(cntnrs)
 
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-b", "--bots", type=int, help="number of containers with bots to start", default=0)
+    parser.add_argument("-s", "--stop", type=int, help="number of containers with bots to start", default=0)
+    parser.add_argument("-i", "--image", type=str, default="sshbot:latest")
+    parser.add_argument("-c", "--container", type=str, default="sshbot")
+    parser.add_argument("--id", type=int, help="last id used for container", default=0)
+    parser.add_argument("-t", "--sleeptime", type=float, default=0.0, help="sleep time between containers startup")
+    args = parser.parse_args()
 
-parser = argparse.ArgumentParser()
-parser.add_argument("-b", "--bots", type=int, help="number of containers with bots to stop", default=0)
-parser.add_argument("-i", "--image", type=str, default="sshbot:latest")
-parser.add_argument("-c", "--container", type=str, default="sshbot")
-parser.add_argument("--bash", type=bool, default=True)
-parser.add_argument("--id", type=int, help="last id used for container", default=0)
-parser.add_argument("-t", "--sleeptime", type=float, default=0.0, help="sleep time between containers startup")
-args = parser.parse_args()
+    print('Image:', args.image)
+    print('Container:', args.container)
 
-if args.bots == 0:
-    run_botnet(args)
-else:
-    stop_botnet(args)
+    if args.bots > 0:
+        run_botnet(args)
+    else:
+        stop_botnet(args)
+
+if __name__ == "__main__":
+    main()
--- sshbot/bot.sh
+++ sshbot/bot.sh
@@ -1,17 +1,35 @@
-#!/bin/sh
-
-SSHHOST=$1
+#!/bin/bash
 DOWORK=1
 
+# http://mywiki.wooledge.org/SignalTrap#When_is_the_signal_handled.3F
 _term () {
 	DOWORK=0
+	[[ $pid ]] && kill $pid
 }
 
 trap _term SIGTERM
 trap _term SIGINT
 
-echo $SSHHOST
+declare -a hostsArr
+hostsCount=0
+while IFS='' read -r line || [[ -n "$line" ]]; do
+	hostsArr[$hostsCount]="$line"
+	((hostsCount++))
+done < ./hosts
+
+if [ $hostsCount == 0 ]; then
+	exit 1
+fi
 
 while [ $DOWORK == 1 ]; do
-	timeout -t 10 sshpass -p passwordddd ssh $SSHHOST -o StrictHostKeyChecking=no UserKnownHostsFile=/dev/null
+	rng2=$(($RANDOM % 10))
+	echo $rng2
+	sleep ${rng2}s & pid=$!
+	wait
+
+	rng=$(($RANDOM % $hostsCount))
+	host=${hostsArr[rng]}
+	echo $host
+	timeout -t 10 sshpass -p passwordddd ssh $host -o StrictHostKeyChecking=no UserKnownHostsFile=/dev/null & pid=$!
+	wait
 done
